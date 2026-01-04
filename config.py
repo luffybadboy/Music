8e4999eb66b9d018d5642e99c412181d5f697468
luffybadboy <luffybadboy17@gmail.com>
2026-01-04 23:31:21 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,20 +9,20 @@ from pyrogram import filters
 load_dotenv()
 
 # ────────────────────── TELEGRAM API ──────────────────────
-API_ID = 22175614
-API_HASH = "5dab14fb645d7c6b5f8d094581192e04"
+API_ID = 12345678
+API_HASH = "abcdefghijklmnopqrstuvwxyz12345678910"
 
 # ────────────────────── BOT TOKEN ─────────────────────────
-BOT_TOKEN = "8121823141:AAE7nHSJuxNMySY3SmtpJ5hFrO_X4RpbPpo"
+BOT_TOKEN = "981298395:BBOOTTOOKKEENN"
 
 # ────────────────────── DATABASE ──────────────────────────
-MONGO_DB_URI = "mongodb+srv://121manokaran:RrQU9aVnFnEwonSX@cluster0.uxizm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_DB_URI = "mongodb+srv://1Mediagroup:ithinkthanoswasright@cluster0.nvidia.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # ────────────────────── BASIC SETTINGS ────────────────────
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
-LOG_GROUP_ID = -1001654008278 #Enter Channel id... Don't enter Group id if done you will get error 
-OWNER_ID = 1491400016
+LOG_GROUP_ID = -1001657008278 #Enter Channel id... Don't enter Group id if done you will get error 
+OWNER_ID = 12345678
 
 # ────────────────────── HEROKU (DISABLED) ─────────────────
 HEROKU_APP_NAME = None
@@ -51,7 +51,7 @@ TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
 TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
 
 # ────────────────────── PYROGRAM SESSIONS ─────────────────
-STRING1 = "AQH-pWEAdpaX9RNqogQdjH2lHvYJ3nk4bldabDXDs4CAb7CxZ0apKRRykSeqtdJ_BEQN9rE7sBRddyJ07FUJGi2xqRIp6pU2UHAwa7gkRW1674Gd-emSNECHyPcWVyzgzvvJm1t3nJ_OEJIZf4q3C_0daxYZypOO7ZROhZn48FSnfkT0BOHDxHcVxYObjk8nRj8q2qsu3dF2Uw_59iQsSu4OCg1cRlVQSiUeCs6U1HW1MPEATUK3casd9_YNDRYM0049etmT6i0VHkQgKD47BIj2fbhMCDscuke70ix3Bj1W_GkJXCU55WgHJGrjbRBdl45LXxw-iPqUEczpOALZD8WqTjby6gAAAAH9Kh65AA"
+STRING1 = "AQH-pWEAdpaX9RNqogQdjH2lHvYJ3nk4bldabDXDs4CAb7CxZ0apKRRykSeqtdJ_BEQN9rE7sBRddyJ07FUJGi2xqRIp6pU2UHAwa7gkRW167"
 STRING2 = None
 STRING3 = None
 STRING4 = None
@@ -66,18 +66,18 @@ autoclean = []
 confirmer = {}
 
 # ────────────────────── IMAGES ────────────────────────────
-START_IMG_URL = "https://i.ibb.co/m50CQT28/x.jpg"
-PING_IMG_URL = "https://i.ibb.co/JRCyfFdp/x.jpg"
-PLAYLIST_IMG_URL = "https://i.ibb.co/dwBc7BXd/x.jpg"
-STATS_IMG_URL = "https://i.ibb.co/sdbRcvLZ/x.jpg"
-TELEGRAM_AUDIO_URL = "https://i.ibb.co/rGSkBj1T/x.jpg"
-TELEGRAM_VIDEO_URL = "https://i.ibb.co/rGSkBj1T/x.jpg"
-STREAM_IMG_URL = "https://i.ibb.co/7dD4Nsbw/x.jpg"
-SOUNCLOUD_IMG_URL = "https://i.ibb.co/7dD4Nsbw/x.jpg"
-YOUTUBE_IMG_URL = "https://i.ibb.co/SXs84HRf/x.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://i.ibb.co/SXs84HRf/x.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://i.ibb.co/SXs84HRf/x.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://i.ibb.co/SXs84HRf/x.jpg"
+START_IMG_URL = ""
+PING_IMG_URL = ""
+PLAYLIST_IMG_URL = ""
+STATS_IMG_URL = ""
+TELEGRAM_AUDIO_URL = ""
+TELEGRAM_VIDEO_URL = ""
+STREAM_IMG_URL = ""
+SOUNCLOUD_IMG_URL = ""
+YOUTUBE_IMG_URL = ""
+SPOTIFY_ARTIST_IMG_URL = ""
+SPOTIFY_ALBUM_IMG_URL = ""
+SPOTIFY_PLAYLIST_IMG_URL = ""
 
 # ────────────────────── FUNCTIONS ─────────────────────────
 def time_to_seconds(time):
@@ -99,6 +99,7 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - SUPPORT_GROUP must start with https://"
 )
+
 
 
 
